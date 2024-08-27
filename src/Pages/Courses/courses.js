2ada1ab22e2bf121df575cbe7c3f4d00508026e5
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-08-27 19:54:24 +1000

    positioned cards
    - added grid layout

---
+++ src/Pages/Courses/courses.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./courses.css";
+
+export default function Courses() {
+  return (
+    <div className="courses">
+      <h1>Training that suits everyone</h1>
+      <h2>From open gym to personal training</h2>
+      <div className="boxWrapper">
+        <div className="boxes">
+          <div className="boxOne">Hello</div>
+
+          <div className="boxTwo">Hello</div>
+
+          <div className="boxThree">Hello</div>
+
+          <div className="boxFour">Hello</div>
+        </div>
+      </div>
+    </div>
+  );
+}
