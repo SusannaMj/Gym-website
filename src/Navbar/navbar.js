c88b1c861b4e926f7e4bc15dee96a44caa92d039
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-08-30 23:35:52 +1000

    styled Navbar

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="NavbarLinks">
-      <Link to="/" className="NavLink">
-        Home
-      </Link>
-      <Link to="/courses" className="NavLink">
-        Courses
-      </Link>
-      <Link to="/pricing" className="NavLink">
-        Pricing
-      </Link>
-      <Link to="/signUp" className="NavLink">
-        SignUp
-      </Link>
+    <div className="NavBar">
+      <div className="NavbarLinks">
+        <Link to="/" className="NavLink">
+          Home
+        </Link>
+        <Link to="/courses" className="NavLink">
+          Courses
+        </Link>
+        <Link to="/pricing" className="NavLink">
+          Pricing
+        </Link>
+        <Link to="/signUp" className="NavLink">
+          SignUp
+        </Link>
+      </div>
     </div>
   );
 }
