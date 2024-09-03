aed1345be6c1cbc71d6f92b67ddbbd5308f92234
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-09-03 22:22:08 +1000

    added subgrid and content

--- src/Pages/Pricing/pricing.js
+++ src/Pages/Pricing/pricing.js
@@ -20,7 +20,64 @@ export default function Pricing() {
         </span>
       </div>
       <div className="Wrapper">
-        <div className=""></div>
+        <div className="pricing-cards">
+          <div className="pricing-card">
+            <div className="subscription-name" id="1">
+              Basic
+              <br />
+              30-day, hassle-free money back guarantee
+            </div>
+            <div className="subscription-price" id="2">
+              $19.99
+              <button>Sign Up</button>
+            </div>
+            <div className="subscription-inclusions" id="3">
+              <ul>
+                <li>24/7 access to the gym</li>
+                <li>1 free class a week</li>
+                <li>discounted merchandise</li>
+              </ul>
+            </div>
+          </div>
+          <div className="pricing-card">
+            {" "}
+            <div className="subscription-name" id="1">
+              Professional
+              <br />
+              30-day, hassle-free money back guarantee
+            </div>
+            <div className="subscription-price" id="2">
+              $24.99
+              <button>Sign Up</button>
+            </div>
+            <div className="subscription-inclusions" id="3">
+              <ul>
+                <li>24/7 access to the gym</li>
+                <li>unlimited free classes</li>
+                <li>discounted merchandise</li>
+              </ul>
+            </div>
+          </div>
+          <div className="pricing-card">
+            {" "}
+            <div className="subscription-name" id="1">
+              Master
+              <br />
+              30-day, hassle-free money back guarantee
+            </div>
+            <div className="subscription-price" id="2">
+              $39.99
+              <button>Sign Up</button>
+            </div>
+            <div className="subscription-inclusions" id="3">
+              <ul>
+                <li>24/7 access to the gym</li>
+                <li>unliited free classes</li>
+                <li>free personal training sessions </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
