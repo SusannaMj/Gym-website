00348aed0b045dbee0603e40213c48206519932b
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-09-03 21:03:52 +1000

    added toggle

--- src/Pages/Pricing/pricing.js
+++ src/Pages/Pricing/pricing.js
@@ -2,5 +2,26 @@ import React from "react";
 import "./pricing.css";
 
 export default function Pricing() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Our pricing</h1>
+      <div className="toggle">
+        <span>
+          Annually
+          <input
+            type="checkbox"
+            className="toggle-checkbox"
+            id="toggle-checkbox"
+          />
+          <label className="toggle-switch" for="toggle-checkbox">
+            {" "}
+          </label>
+          Monthly
+        </span>
+      </div>
+      <div className="Wrapper">
+        <div className=""></div>
+      </div>
+    </div>
+  );
 }
