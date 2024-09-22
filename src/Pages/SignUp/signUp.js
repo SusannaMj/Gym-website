7d866d7b188bb4f9fa75bc98d97ccd019036f509
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-09-22 21:04:17 +1000

    added signup form and added basic styling

--- src/Pages/SignUp/signUp.js
+++ src/Pages/SignUp/signUp.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./signUp.css";
 
 export default function SignUp() {
-  return <div>Hello</div>;
+  return (
+    <div className="signUpWrapper">
+      <div className="catchPhrase">
+        Ready to take the leap into a health´ier, happier life?
+      </div>
+      <div className="signUpForm">
+        <div>
+          <input type="text" placeholder="FIRST NAME" />
+          <input type="text" placeholder="LAST NAME" />
+          <input type="email" placeholder="EMAIL" />
+          <input type="password" placeholder="PASSWORD" />
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 }
