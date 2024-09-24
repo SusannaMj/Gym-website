e1a56b190e67ddd5a717ecf08a8c7592d02f33e0
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-09-24 23:31:10 +1000

    styled sign Up page

--- src/Pages/SignUp/signUp.js
+++ src/Pages/SignUp/signUp.js
@@ -5,15 +5,35 @@ export default function SignUp() {
   return (
     <div className="signUpWrapper">
       <div className="catchPhrase">
-        Ready to take the leap into a health´ier, happier life?
+        <h2>Ready to take the leap into a healthier, happier life?</h2>
+        <p>
+          Sign up and join us and milions of others - choose a package that best
+          suits your needs and we will be in contact soon
+        </p>
       </div>
       <div className="signUpForm">
-        <div>
+        <button
+          className="tryButton"
+          style={{ backgroundColor: "var(--purple)", color: "var(--white)" }}
+        >
+          Try it free for 7 days{" "}
+          <sapn style={{ opacity: "0.6" }}>
+            | full price as per chosen package thereafter
+          </sapn>
+        </button>
+        <div className="signUpMain">
           <input type="text" placeholder="FIRST NAME" />
           <input type="text" placeholder="LAST NAME" />
           <input type="email" placeholder="EMAIL" />
           <input type="password" placeholder="PASSWORD" />
-          <button>Sign Up</button>
+          <button
+            style={{
+              backgroundColor: "var(--dark-green)",
+              color: "var(--white)",
+            }}
+          >
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
