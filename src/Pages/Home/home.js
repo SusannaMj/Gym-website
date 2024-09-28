92f3a1a9c966b8b291d2e7ba0ca1433c3569f4ce
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-09-28 23:52:59 +1000

    add additional stripes to background overlay

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -4,28 +4,30 @@ import "./home.css";
 export default function Home() {
   return (
     <div className="homepageBody">
-      <div className="homepageHero">
-        <h1>Become Fitter, Stronger & more confident!</h1>
-        <h2>Get started with us from $5 AUD a week</h2>
-        <button>Pricing</button>
-        <button>Learn More</button>
-        <h3>
-          More than 1 million people are training with us in one of our programs
-          all over Australia and New Zealand
-        </h3>
-        <p>
-          <span>√ Daily Classes</span>
-          <span>√ personal Training</span>
-          <span>√ Modern Equipment</span>
-        </p>
-      </div>
-
-      <div>Start training with us today</div>
-      <footer className="contact">
-        <a href="">Linkedin</a>
-        <a href="">Facebook</a>
-        <a href="">Twitter</a>
-      </footer>
+      <div className="overlay">
+        <div className="stripe"></div>
+        <div className="homepageHero">
+          <h1>Become Fitter, Stronger & more confident!</h1>
+          <h2>Get started with us from $5 AUD a week</h2>
+          <button>Pricing</button>
+          <button>Learn More</button>
+          <h3>
+            More than 1 million people are training with us in one of our
+            programs all over Australia and New Zealand
+          </h3>
+          <p>
+            <span>√ Daily Classes</span>
+            <span>√ personal Training</span>
+            <span>√ Modern Equipment</span>
+          </p>{" "}
+          <footer className="contact">
+            <a href="">Linkedin</a>
+            <a href="">Facebook</a>
+            <a href="">Twitter</a>
+          </footer>
+        </div>
+        <div className="stripeTwo"></div>{" "}
+      </div>{" "}
     </div>
   );
 }
