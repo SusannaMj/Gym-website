e5b819d4d3d353b663ff61862ed419f497b58a17
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-09-29 00:40:26 +1000

    added LinkedIn and Twitte ricon to facebook icon

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -3,6 +3,8 @@ import "./home.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
@@ -27,11 +29,15 @@ export default function Home() {
         <div className="stripeTwo"></div>{" "}
       </div>{" "}
       <footer className="contact">
-        <a href="">Linkedin</a>
+        <a href="">
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
         <a href="#" rel="noreferrer">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
-        <a href="">Twitter</a>
+        <a href="">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
       </footer>
     </div>
   );
